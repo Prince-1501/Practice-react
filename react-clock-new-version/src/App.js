8145837fd66713cd53c6be03004fc55c4d04884b
Prince-1501 <princekumarrpcsvm10@gmail.com>
2020-05-23 20:35:29 +0530

    add css in clock

--- react-clock-new-version/src/App.js
+++ react-clock-new-version/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import classes from './App.module.css';
 import Logo from './bg-image.svg';
-import Grid from './assets/css/Grid.css';
+import grid from './assets/css/Grid.css';
 
 
 class App extends Component {
@@ -17,26 +17,27 @@ class App extends Component {
     });
   }
 
-
   render(){
     let date = new Date();
     let time = date.toLocaleTimeString();
 
     let showDetail = false;
+    let message = "Show Date";
     if(this.state.showDay){
       const day = date.toLocaleDateString();
       showDetail = (day);
+      message = "Remove Date";
     }
 
     return (
       <div className={classes.App}>
           <h1 className={classes.h1}>Your Time is here </h1>
-          <img  src={Logo} width="500" height="600" className={`${Grid.col} ${Grid.span-1-of-2}`}/>
-          <button className={`${classes.btn} ${classes.some}`} onClick={this.showDayHandler}> Choose me! </button>
+          <img  src={Logo} width="500" height="600"/>
           <div className={classes.time}>
-            <div>{time}</div>
-            <div>{showDetail}</div>
+            <div className={classes.showTime}>{time}</div>
+            <div className={classes.showTime}>{showDetail}</div>
           </div>
+          <button className={classes.btn} onClick={this.showDayHandler}> {message} </button>
       </div>
     );
   }
