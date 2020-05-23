1dfd2b0befc5331bf2f7c37a450d0b35d780580a
Prince-1501 <princekumarrpcsvm10@gmail.com>
2020-05-23 12:53:11 +0530

    start clock project

---
+++ react-clock-new-version/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+        <h1 className={classes.h1}>Prince kumar</h1>
+        <p className={classes.some}>This is a lorem ipsum and the things like what to do </p>
+    </div>
+  );
+}
+
+export default App;
