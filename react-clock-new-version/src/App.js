c010bde5d0e936f8d519a72ed6b5d63df1006133
Prince-1501 <princekumarrpcsvm10@gmail.com>
2020-05-23 13:41:19 +0530

    add some background theme

--- react-clock-new-version/src/App.js
+++ react-clock-new-version/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './App.module.css';
+import Logo from './bg-image.svg';
 
-function App() {
-  return (
-    <div className={classes.App}>
-        <h1 className={classes.h1}>Prince kumar</h1>
-        <p className={classes.some}>This is a lorem ipsum and the things like what to do </p>
-    </div>
-  );
+
+class App extends Component {
+
+  state= {
+    showTime : false
+  }
+
+
+
+  render(){
+    return (
+      <div className={classes.App}>
+          <h1 className={classes.h1}>Your Time is here </h1>
+          <img  src={Logo} width="500" height="600"/>
+
+          <button>Detail View </button>
+
+      </div>
+    );
+  }
 }
 
 export default App;
