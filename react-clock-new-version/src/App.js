e6cbca27653f5ce9fffb6bebc0892b3ddf9639b4
Prince-1501 <princekumarrpcsvm10@gmail.com>
2020-05-23 14:06:12 +0530

    start clock project

--- react-clock-new-version/src/App.js
+++ react-clock-new-version/src/App.js
@@ -6,19 +6,37 @@ import Logo from './bg-image.svg';
 class App extends Component {
 
   state= {
-    showTime : false
+    showDay : false
+  }
+
+  showDayHandler = () =>{
+    const day = this.state.showDay;
+    this.setState({
+      showDay: !day
+    });
   }
 
 
-
   render(){
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+
+    let showDetail = false;
+    if(this.state.showDay){
+      const day = date.toLocaleDateString();
+      showDetail = (day);
+    }
+
     return (
       <div className={classes.App}>
           <h1 className={classes.h1}>Your Time is here </h1>
           <img  src={Logo} width="500" height="600"/>
 
-          <button>Detail View </button>
-
+          <button onClick={this.showDayHandler}>Detail View </button>
+          <div className={classes.time}>
+            <div>{time}</div>
+            <div>{showDetail}</div>
+          </div>
       </div>
     );
   }
